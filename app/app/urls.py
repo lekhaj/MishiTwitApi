435dd5e7b7d3d33f0123baf8a5a44398aab76541
Lekhaj <lekhaj123btmn@gmail.com>
2021-02-07 08:49:41 +0530

    Configured postgres db.

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ app/mishi_tweet_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MishiTweetApiConfig(AppConfig):
+    name = 'mishi_tweet_api'
